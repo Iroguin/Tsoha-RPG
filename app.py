f77845fe4745d592a2a32d430a467c7451125690
iroguin <143116756+Iroguin@users.noreply.github.com>
2024-04-07 22:42:52 +0300

    update

--- app.py
+++ app.py
@@ -7,36 +7,24 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
+
+
 app = Flask(__name__, template_folder="templates")
 app.secret_key = getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
 
 
+
+
 #Essentials
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-@app.route("/login",methods=["POST"])
-def login():
-    username = request.form["username"]
-    password = request.form["password"]
-    # TODO: check username and password
-    session["username"] = username
-    return redirect("/")
-
-@app.route("/logout")
-def logout():
-    del session["username"]
-    return redirect("/")
 
 
 #######################
 
 
 #####################
+
 
 
 
